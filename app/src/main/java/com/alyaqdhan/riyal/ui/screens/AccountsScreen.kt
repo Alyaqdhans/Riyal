ccabf9a2d824630ea6949ee03accbaf445962786
sohaib aladawi <sohaib.aladawi@gmail.com>
2026-08-27 06:25:45 +0400

    ui: drop the imports these screens stopped using

--- app/src/main/java/com/alyaqdhan/riyal/ui/screens/AccountsScreen.kt
+++ app/src/main/java/com/alyaqdhan/riyal/ui/screens/AccountsScreen.kt
@@ -23,7 +23,6 @@ import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.FilledTonalButton
